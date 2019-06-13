17e1ab6752ead7bd114889e59d236a06bdf82284
Blessed Sibanda <n01519451g@students.nust.ac.zw>
2019-06-13 15:15:28 +0200

    crud functionality

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ var logger = require('morgan');
 const hbs = require('hbs');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+// var usersRouter = require('./routes/users');
+const notesRouter = require('./routes/notes');
 
 var app = express();
 
@@ -22,7 +23,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// app.use('/users', usersRouter);
+app.use('/notes', notesRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
