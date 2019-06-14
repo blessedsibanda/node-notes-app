89a2e1411d707904cfc38b0a4ca76a5ccb48c970
Blessed Sibanda <n01519451g@students.nust.ac.zw>
2019-06-14 14:04:14 +0200

    use data storages

--- routes/notes.mjs
+++ routes/notes.mjs
@@ -3,62 +3,82 @@ import * as notes from '../models/notes.mjs'
 
 export const router = express.Router();
 
+import DBG from 'debug'
+
+const debug = DBG('notes:notes-routes')
+const error = DBG('notes:error-routes')
+
 
 
 // Add Note (create)
 router.get('/add', (req, res, next) => {
-    res.render('noteedit', {
-        title: 'Add a Note',
-        docreate: true,
-        notekey: '', note: undefined
-    });
+    try {
+        res.render('noteedit', {
+            title: 'Add a Note',
+            docreate: true,
+            notekey: '',
+            note: undefined
+        });
+    } catch (e) { next(e) }
 });
 
 // Save Note (update)
 router.post('/save', async (req, res, next)=> {
-    var note;
-    if (req.body.docreate === 'create') {
-        note = await notes.create(req.body.notekey, req.body.title,
-            req.body.body);
-    } else {
-        note = await notes.update(req.body.notekey, req.body.title,
-            req.body.body);
-    }
-    res.redirect('/notes/view?key='+ req.body.notekey)
+    try {
+        var note;
+        if (req.body.docreate === 'create') {
+            debug('CREATING note')
+            note = await notes.create(req.body.notekey, req.body.title,
+                req.body.body);
+        } else {
+            debug('UPDATING note')
+            note = await notes.update(req.body.notekey, req.body.title,
+                req.body.body);
+        }
+        res.redirect('/notes/view?key='+ req.body.notekey)
+    } catch(e) { next(e); }
 })
 
 // Read Note (read)
 router.get('/view', async (req, res, next) => {
-    var note = await notes.read(req.query.key);
-    res.render('noteview', {
-        title: note ? note.title : '',
-        notekey: req.query.key, 
-        note: note
-    });
+    try {
+        var note = await notes.read(req.query.key);
+        res.render('noteview', {
+            title: note ? note.title : '',
+            notekey: req.query.key, 
+            note: note
+        });
+    } catch(e) { next(e); }
 });
 
 // Edit note (update)
 router.get('/edit', async (req, res, next) => {
-    var note = await notes.read(req.query.key);
-    res.render('noteedit', {
-        title: note ? ("Edit " + note.title) : "Add a Note",
-        docreate: false,
-        notekey: req.query.key,
-        note: note
-    });
+    try {
+        var note = await notes.read(req.query.key);
+        res.render('noteedit', {
+            title: note ? ("Edit " + note.title) : "Add a Note",
+            docreate: false,
+            notekey: req.query.key,
+            note: note
+        });
+    } catch(e) { next(e); }
 });
 
 // Ask to delete note (destroy)
 router.get('/destroy', async (req, res, next) => {
-    var note = await notes.read(req.query.key);
-    res.render('notedestroy', {
-        title: note ? note.title : '',
-        notekey: req.query.key, note: note
-    });
+    try {
+        var note = await notes.read(req.query.key);
+        res.render('notedestroy', {
+            title: note ? note.title : '',
+            notekey: req.query.key, note: note
+        });
+    } catch(e) { next(e); } 
 });
 
 // Really destroy note (destroy)
 router.post('/destroy/confirm', async (req, res, next) => {
-    await notes.destroy(req.body.notekey);
-    res.redirect('/')
+    try {
+        await notes.destroy(req.body.notekey);
+        res.redirect('/')
+    } catch(e) { next(e); } 
 })
